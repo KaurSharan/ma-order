0243c402aefc18af0d7fcd434e29428845d258bd
Sharan <jatti105@gmail.com>
2016-10-21 07:25:27 +0530

    changes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+	rescue_from ActionController::InvalidAuthenticityToken, with: :destroy
+	def create
+		auth = request.env["omniauth.auth"]
+		session[:omniauth] = auth.except('extra')
+		user = User.sign_in_from_omniauth(auth)
+
+		session[:user_id] = user.id
+		session[:email] = user.email
+		redirect_to root_url
+	end
+
+	def destroy
+		session[:email] = nil
+	    session[:id] = nil
+	    session[:user_id] = nil
+	    session[:omniauth] = nil
+	    session[:name] = nil
+	    session[:products] = nil
+	    session[:cart_size] = nil
+	     sleep 3
+		redirect_to root_url, notice: "SIGNED OUT"
+	end
+end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook,'1759792904270188','dabb9cb79278b70bd73dde42c70bff79'
+end
