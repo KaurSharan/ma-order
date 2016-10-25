df627661a8561256d10a87ac5285f34c8f13f000
Sharan <jatti105@gmail.com>
2016-10-25 05:32:38 +0530

    changes in user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-	require 'digest/sha1'
-	 before_save :do_hashing
+	# require 'digest/sha1'
+	#  before_save :do_hashing
 	has_one :userdetail
 	has_many :ordermaster
 	has_many :product
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 	validates_uniqueness_of :email
 	VALID_EMAIL_REGEX = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-	  validates_length_of :password, minimum: 6 
+	  # validates_length_of :password, minimum: 6 
 
 	 	def hash_password(password)
 	 		return Digest::SHA1.hexdigest(password)
@@ -22,7 +22,8 @@ class User < ActiveRecord::Base
 
 	 def self.authenticate_user(email,password)
     	@user = User.find_by_email(email)
-		if @user && @user.password ==  self.new.hash_password(password)
+		if @user && @user.password == password 
+			# self.new.hash_password(password)
 			return @user
 		else
 			return false
@@ -37,7 +38,7 @@ class User < ActiveRecord::Base
 	    user.email = auth['info']['email']
 	    user.uid = auth['uid']
 	    user.provider = auth['provider']
-	    user.password = "workingfine"
+	    # user.password = "workingfine"
 	    user.save!
 	    user
 		# find_by(provider: auth['provider'],uid: auth['uid']) || create_user_from_omniauth(auth)
